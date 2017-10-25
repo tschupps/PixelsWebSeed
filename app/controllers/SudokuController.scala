27f141d62188d39ad1afe14211cb56f0576175ca
tschupps <hanna@schredl.eu>
2017-10-25 14:33:27 +0200

    fixed dependencies from artifacts

--- app/controllers/SudokuController.scala
+++ app/controllers/SudokuController.scala
@@ -7,19 +7,18 @@ import de.htwg.se.sudoku.Sudoku
 import de.htwg.se.sudoku.controller.controllerComponent.GameStatus
 
 import de.htwg.se.pixels.aview.gui.Gui
-import de.htwg.se.pixels.aview.tui.Tui
 import de.htwg.se.pixels.controller.Controller
 import de.htwg.se.pixels.model.impl.Grid
 
 
 @Singleton
 class SudokuController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
   val grid = new Grid(1,1)
   val pixels = Controller(grid,grid)
-  //val tui = Tui(pixels)
   val gui = new Gui(pixels)
-
   gui.visible = true
+
   val gameController = Sudoku.controller
   def tui =  gameController.gridToString + GameStatus.message(gameController.gameStatus)
 
