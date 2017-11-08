91b3075b08d93ee295af58614dcb2ae6d11066d1
tschupps <hanna@schredl.eu>
2017-11-08 11:53:11 +0100

    added Pixel Controller instead of SudokuController

---
+++ app/controllers/PixelController.scala
@@ -0,0 +1,37 @@
+package controllers
+
+import javax.inject._
+
+import play.api.mvc._
+//import de.htwg.se.sudoku.Sudoku
+//import de.htwg.se.sudoku.controller.controllerComponent.GameStatus
+
+import de.htwg.se.pixels.aview.gui.Gui
+import de.htwg.se.pixels.aview.tui.Tui
+import de.htwg.se.pixels.controller.Controller
+import de.htwg.se.pixels.model.impl.Grid
+
+
+@Singleton
+class PixelController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+  val grid = new Grid(1,1)
+  val pixels = Controller(grid,grid)
+
+  val tui = new Tui(pixels)
+  val gui = new Gui(pixels)
+  gui.visible = true
+
+  //val gameController = Sudoku.controller
+  //def tui =  gameController.gridToString + GameStatus.message(gameController.gameStatus)
+
+  def sudoku = Action {
+    Ok(tui.printTui())
+  }
+
+  def help = Action {
+
+    //gameController.createNewGrid
+    Ok(views.html.welcome("welcomeTest"))
+  }
+}
