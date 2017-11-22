0455df69e90092d213c0246a36a64a087c7d4ce0
Jessica Kaechele <jessica.kaechele@web.de>
2017-11-22 14:01:43 +0100

    added grid, and tried to use jquery

--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,3 +1,9 @@
 if (window.console) {
   console.log("Welcome to your Play application's JavaScript!");
 }
+$(document).ready(function () {
+$(".cell").on("click", function() {
+    $(this).css("background", "red");
+});
+
+});
